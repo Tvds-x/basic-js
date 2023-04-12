922d732e11f1abafea82daf1ff87327716947f13
Tvds-x <tvds.x@yandex.ru>
2023-04-12 19:03:56 +0300

    feat: 35 passing

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,11 +17,17 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity ) {
+  if ((isNaN(+sampleActivity)) || !(typeof sampleActivity === 'string') || sampleActivity === '' || +sampleActivity <=0 || +sampleActivity >=MODERN_ACTIVITY){
+    return false
+  }
+  result = Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / (0.693/HALF_LIFE_PERIOD))
+  return result
+
 }
 
 module.exports = {
   dateSample
 };
+
+console.log(dateSample('-52892'));
--- src/count-cats.js
+++ src/count-cats.js
@@ -14,10 +14,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(matrix) {
+  // throw new NotImplementedError('Not implemented');
+  // console.log(matrix);
+  result = 0;
+  for (el of matrix){
+    for (cat of el){
+      if (cat === '^^'){
+        result++
+      }
+    }
+  }
+  return result;
 }
+
+countCats([
+   [0, 1, '^^'],
+   [0, '^^', 2],
+   ['^^', 1, 2]
+  ])
 
 module.exports = {
   countCats
